254e27d1195d6b064c094890e1c6947d8ab6eeb3
kiforsbe <kim.forsberg@gmail.com>
2026-07-25 10:26:45 +0200

    feat: update frequencies for symbols in all reels to enhance gameplay balance and RTP 96%

--- games/fruitmachine/game.js
+++ games/fruitmachine/game.js
@@ -58,44 +58,42 @@ export const PAYTABLE = {
 // are wild symbols and are always meant to stay fixed, independent of payout ordering.
 // Note that REEL_1 does not contain the star or strawberry symbols.
 export const FREQUENCY_REEL1 = {
-  bar:        { frequency: 16.0 },
-  clover:     { frequency: 16.0 },
-  pear:       { frequency: 16.0 },
-  melon:      { frequency: 20.0 },
-  grapes:     { frequency:  4.0 },
-  plum:       { frequency: 16.0 },
-  orange:     { frequency:  4.0 },
-  cherries:   { frequency:  4.0 },
-  star:       { frequency:  0.0, fixed: true },
-  strawberry: { frequency:  0.0, fixed: true },
+  bar:        { frequency: 24.5 },
+  clover:     { frequency: 20.1 },
+  pear:       { frequency: 13.2 },
+  melon:      { frequency: 17.6 },
+  grapes:     { frequency: 3.5 },
+  plum:       { frequency: 10.1 },
+  orange:     { frequency: 3.5 },
+  cherries:   { frequency: 3.5 },
+  star:       { frequency: 0.0, fixed: true },
+  strawberry: { frequency: 0.0, fixed: true },
 };
 
-// This reel contains the same symbols as reel 1, but with different frequencies to create a different distribution of symbols.
 export const FREQUENCY_REEL2 = {
-  bar:        { frequency:  8.0 },
-  clover:     { frequency:  8.0 },
-  pear:       { frequency:  8.0 },
-  melon:      { frequency: 20.0 },
-  grapes:     { frequency: 16.0 },
-  plum:       { frequency: 16.0 },
-  orange:     { frequency:  8.0 },
-  cherries:   { frequency: 12.0 },
-  star:       { frequency:  0.0, fixed: true },
-  strawberry: { frequency:  0.0, fixed: true },
+  bar:        { frequency: 8.9 },
+  clover:     { frequency: 10.5 },
+  pear:       { frequency: 10.5 },
+  melon:      { frequency: 11.7 },
+  grapes:     { frequency: 12.3 },
+  plum:       { frequency: 13.0 },
+  orange:     { frequency: 13.6 },
+  cherries:   { frequency: 15.5 },
+  star:       { frequency: 0.0, fixed: true },
+  strawberry: { frequency: 0.0, fixed: true },
 };
 
-// This reel contains additional symbols (star and strawberry) and different frequencies to create a different distribution of symbols.
 export const FREQUENCY_REEL3 = {
-  bar:        { frequency: 16.0 },
-  clover:     { frequency: 16.0 },
-  pear:       { frequency:  8.0 },
-  melon:      { frequency: 16.0 },
-  grapes:     { frequency: 12.0 },
-  plum:       { frequency: 12.0 },
-  orange:     { frequency:  4.0 },
-  cherries:   { frequency:  4.0 },
-  star:       { frequency: 24.0 },
-  strawberry: { frequency:  4.0 },
+  bar:        { frequency: 22.8 },
+  clover:     { frequency: 14.3 },
+  pear:       { frequency: 6.2 },
+  melon:      { frequency: 18.0 },
+  grapes:     { frequency: 3.3 },
+  plum:       { frequency: 9.7 },
+  orange:     { frequency: 3.8 },
+  cherries:   { frequency: 3.3 },
+  star:       { frequency: 28.3, min: 20, max: 30 },
+  strawberry: { frequency: 6.3, min: 2, max: 6 },
 };
 
 // Generate the reel strips based on the frequency tables and seeds.
